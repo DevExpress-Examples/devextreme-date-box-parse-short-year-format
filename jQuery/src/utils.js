e72ec4305b927adadccbd61035928072e15812df
Diana Gromova <16adianay@gmail.com>
2024-01-16 19:18:20 +0400

    jQuery example

--- jQuery/src/utils.js
+++ jQuery/src/utils.js
@@ -1,11 +1,9 @@
 const parser = (value) => {
-    const parts = value.split('/');
-    if (parts.length !== 3) return;
-    let year = Number(parts[2]);
-    if (year < 100) year += 2000;
-    return new Date(year, Number(parts[0]) - 1, Number(parts[1]));
-}
+  const parts = value.split('/');
+  if (parts.length !== 3) return;
+  let year = Number(parts[2]);
+  if (year < 100) year += 2000;
+  return new Date(year, Number(parts[0]) - 1, Number(parts[1]));
+};
 
-const formatter = (value) => {
-    return value.toLocaleDateString();
-}
+const formatter = (value) => value.toLocaleDateString();
