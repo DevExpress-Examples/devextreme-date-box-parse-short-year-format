b1a2ca51da3a565fa4ff260c6e0aef6184a9a598
Diana Gromova <16adianay@gmail.com>
2024-01-16 19:23:49 +0400

    Update utils.js

--- jQuery/src/utils.js
+++ jQuery/src/utils.js
@@ -1,6 +1,6 @@
 const parser = (value) => {
   const parts = value.split('/');
-  if (parts.length !== 3) return;
+  if (parts.length !== 3) return value;
   let year = Number(parts[2]);
   if (year < 100) year += 2000;
   return new Date(year, Number(parts[0]) - 1, Number(parts[1]));
