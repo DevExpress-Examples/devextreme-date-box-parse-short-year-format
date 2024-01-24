41a2bb1553240649eaaf8eac5fe856b1d9f4f325
Diana Gromova <16adianay@gmail.com>
2024-01-24 18:04:12 +0400

    React example

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,16 +1,47 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
+import DataGrid, { Column, Editing } from 'devextreme-react/data-grid';
+import DateBox from 'devextreme-react/date-box';
+import { Format } from 'devextreme/localization';
+import { employees } from './data';
+import { formatter, parser } from './utils';
 import './App.css';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import Button from 'devextreme-react/button';
+
+const now = new Date();
+
+const format: Format = {
+  parser: (val) => parser(val),
+  formatter: (val) => formatter(val),
+};
+
+const editorOptions: { displayFormat: Format } = {
+  displayFormat: {
+    parser: (val) => parser(val),
+    formatter: (val) => formatter(val),
+  },
+};
 
 function App(): JSX.Element {
-  var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, [setCount]);
   return (
     <div className="main">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
+      <DateBox
+        value={now}
+        type="date"
+        label="Date with the short year"
+        displayFormat={format}
+      />
+      <DataGrid
+        id="grid"
+        dataSource={employees}
+        width={400}
+        keyExpr='ID'
+        showBorders={true}
+      >
+        <Editing mode="cell" allowUpdating={true} />
+        <Column dataField='FirstName' />
+        <Column dataField="LastName" />
+        <Column dataField='BirthDate' dataType='date' editorOptions={editorOptions} />
+      </DataGrid>
     </div>
   );
 }
