5a0f9f9a644dfe747622151555891827a8a65ad0
Diana Gromova <16adianay@gmail.com>
2024-01-25 19:21:40 +0400

    Angular example

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DxButtonModule } from 'devextreme-angular/ui/button';
+import { DxDateBoxModule, DxDataGridModule } from 'devextreme-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -11,7 +11,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DxButtonModule,
+    DxDateBoxModule,
+    DxDataGridModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
