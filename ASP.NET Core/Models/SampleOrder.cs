eb6c54ba32b71742dd7783d1fe18ba64bd5988d5
Diana Gromova <16adianay@gmail.com>
2024-01-29 20:46:36 +0400

    ASP.NET Example

--- ASP.NET Core/Models/SampleOrder.cs
+++ ASP.NET Core/Models/SampleOrder.cs
@@ -5,12 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ASP_NET_Core.Models {
-    public class SampleOrder {
-        public int OrderID { get; set; }
-        public DateTime OrderDate { get; set; }
-        public string CustomerID { get; set; }
-        public string CustomerName { get; set; }
-        public string ShipCountry { get; set; }
-        public string ShipCity { get; set; }
+    public class Employee {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
     }
 }
