760413da2a582bd5d9b921e7c239376d9102c1e0
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-09-24 15:15:24 +0300

    jQuery: JavaScript modernization with const/let and proper DateBox parsing functionality

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,50 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
+  const now = new Date();
+
+  const parseSelectBox = $('#parse-behavior').dxSelectBox({
+    width: 300,
+    items,
+    displayExpr: 'text',
+    valueExpr: 'value',
+    value: 'javascript',
+  }).dxSelectBox('instance');
+
+  $('#date').dxDateBox({
+    type: 'date',
+    label: 'Date with the short year',
+    labelMode: 'outside',
+    displayFormat: {
+      parser: (val) => parser(val, parseSelectBox.option('value')),
+      formatter: (val) => formatter(val),
     },
+    value: now,
+  });
+
+  $('#grid').dxDataGrid({
+    width: 400,
+    dataSource: employees,
+    keyExpr: 'ID',
+    showBorders: true,
+    paging: {
+      enabled: false,
+    },
+    editing: {
+      mode: 'cell',
+      allowUpdating: true,
+    },
+    columns: [
+      'FirstName',
+      'LastName', {
+        dataField: 'HireDate',
+        dataType: 'date',
+        editorOptions: {
+          displayFormat: {
+            parser: (val) => parser(val, parseSelectBox.option('value')),
+            formatter: (val) => formatter(val),
+          },
+        },
+        width: 125,
+      },
+    ],
   });
 });
